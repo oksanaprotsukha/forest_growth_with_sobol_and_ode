1e7714915714b3370e6d4b0c6661408cf4c225a1
oksanaprotsukha <ok.protsukha@gmail.com>
2025-05-17 18:38:36 -0700

    Forest growth sensitivity analysis with ode and sobol

---
+++ R/forest_growth.R
@@ -0,0 +1,34 @@
+#' Calculate forest growth rate using a differential equation
+#'
+#' @param C - the size of the forest in units of carbon
+#' @param parms - as list with three values, r, K, harv
+#' @param r - an exponential growth rate during a young forest stage before a canopy closure threshold is reached
+#' @param g - a linear growth rate after a canopy closure threshold is reached
+#' @param K - carrying capacity in units of carbon
+#' @param canopy_thresh - canopy closure threshold in units of carbon 
+#' @param t - time period for which forest growth is analysed, in years
+#'
+#' @return derivative of forest growth with time
+#' @export
+#'
+#' @examples
+
+forest_growth <- function(t, y, parms){
+  # Current carbon value
+  C <- y[1]  
+  
+  # Extract parameters from the list
+  K <- parms$K
+  r <- parms$r
+  g <- parms$g
+  canopy_thresh <- parms$canopy_thresh
+  
+  if (C < canopy_thresh) {
+    forest_dt <- r * C
+  } else {
+    forest_dt <- g * (1 - C/K)
+  }
+  
+  return(list(forest_dt))
+}
+
